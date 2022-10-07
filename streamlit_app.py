117ecf4129cdadf86c20cd43c163cd922126cc8a
Pravallika Motupalli <114479124+pravallikamknsl@users.noreply.github.com>
2022-10-07 12:20:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('eggs')
+
+
